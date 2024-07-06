c77697a36fc5c7d4b6808103ff6506762ce66a94
Sifat-Ikram <sifatikram@gmail.com>
2024-07-06 21:47:22 +0600

    developing backend started

---
+++ index.js
@@ -0,0 +1,186 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+// var jwt = require("jsonwebtoken");
+const cors = require("cors");
+const port = process.env.PORT || 4321;
+
+// middleware
+app.use(
+  cors()
+);
+app.use(express.json());
+
+// {
+//     origin: ["http://localhost:5173", "https://task-master-client96.web.app"],
+//     credentials: true,
+//   }
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://task-master-client96.web.app');
+//   // Additional headers you may need
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+const verifyToken = (req, res, next) => {
+  // console.log("inside middleware", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Access forbidden" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "Access forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrqljyn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const taskCollection = client.db("taskMaster").collection("task");
+    const userCollection = client.db("taskMaster").collection("user");
+    const bookingCollection = client.db("taskMaster").collection("booking");
+    const runningCollection = client.db("taskMaster").collection("running");
+    const completedCollection = client
+      .db("task-master")
+      .collection("completed");
+
+    // // jwt api
+    // app.post("/jwt", (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+    //     expiresIn: "1h",
+    //   });
+    //   res.send({ token });
+    // });
+
+    // user api
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const ifExist = await userCollection.findOne(query);
+      if (ifExist) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // task api
+    app.get("/task", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //   bookings api
+    app.post("/bookings", async (req, res) => {
+      const bookingItem = req.body;
+      const result = await bookingCollection.insertOne(bookingItem);
+      res.send(result);
+    });
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //   running api
+    app.post("/running", async (req, res) => {
+      const runningItem = req.body;
+      const result = await runningCollection.insertOne(runningItem);
+      res.send(result);
+    });
+    app.get("/running", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await runningCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/running/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await runningCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //   completed api
+    app.post("/completed", async (req, res) => {
+      const completedItem = req.body;
+      const result = await completedCollection.insertOne(completedItem);
+      res.send(result);
+    });
+    app.get("/completed", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await completedCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/completed/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await completedCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("task master is mastering");
+});
+
+app.listen(port, () => {
+  console.log(`task master is mastering through port ${port}`);
+});
